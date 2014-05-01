464ddc64a7efc95e4f1603bd8475b46ddf3b1f77
Moumou38 <moumou38@msn.com>
2014-05-01 23:14:03 +0200

    bool isAnagram - OK

--- src/Jeu/Anagramme.java
+++ src/Jeu/Anagramme.java
@@ -3,6 +3,7 @@ package Jeu;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import Briques.Case;
 import Jeu.Constants.Key;
@@ -10,6 +11,7 @@ import Jeu.Constants.Key;
 public class Anagramme extends GameMode {
 
 	private String currentWord;
+	private String bestWord; 
 	private long timeLeft;
 	private int currentrow; 
 	private int nbLetters ;
@@ -23,6 +25,52 @@ public class Anagramme extends GameMode {
 		found = false; 
 		currentRow = nbrow;
 		
+	}
+	
+	public String CheckAnagramme(int currentRow) {
+		String tmp = null; 
+		
+		return tmp; 
+		
+	}
+	
+	/*
+	public static ArrayList<String> permutations(String s) {
+	    ArrayList<String> out = new ArrayList<String>();
+	    if (s.length() == 1) {
+	        out.add(s);
+	        return out;
+	    }
+	    char first = s.charAt(0);
+	    String rest = s.substring(1);
+	    for (String permutation : permutations(rest)) {
+	        out.addAll(insertAtAllPositions(first, permutation));
+	    }
+	    return out;
+	}
+	public static ArrayList<String> insertAtAllPositions(char ch, String s) {
+	    ArrayList<String> out = new ArrayList<String>();
+	    for (int i = 0; i <= s.length(); ++i) {
+	        String inserted = s.substring(0, i) + ch + s.substring(i);
+	        out.add(inserted);
+	    }
+	    return out;
+	}*/
+	
+	public static boolean isAnagram(String s1, String s2){
+
+        // Early termination check, if strings are of unequal lengths,
+        // then they cannot be anagrams
+        if ( s1.length() != s2.length() ) {
+            return false;
+        }
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        String sc1 = new String(c1);
+        String sc2 = new String(c2);
+        return sc1.equals(sc2);
 	}
 	
 	public void click(int x, int y) {
